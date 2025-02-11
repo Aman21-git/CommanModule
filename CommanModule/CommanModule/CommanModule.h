f350526dd9d977c3618e467be9704fcf7dabe38a
Aman21-git <aman.arvaan@gmail.com>
2025-02-11 16:20:24 +0530

    init commit

---
+++ CommanModule/CommanModule/CommanModule.h
@@ -0,0 +1,18 @@
+//
+//  CommanModule.h
+//  CommanModule
+//
+//  Created by Arvaan Technolab on 2/11/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CommanModule.
+FOUNDATION_EXPORT double CommanModuleVersionNumber;
+
+//! Project version string for CommanModule.
+FOUNDATION_EXPORT const unsigned char CommanModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommanModule/PublicHeader.h>
+
+
